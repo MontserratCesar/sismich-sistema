3b7cdcd54fa7e9c0986006e6df2ac7139bb1e481
MontserratCesar <morecesar123ale@gmail.com>
2026-02-20 14:31:03 -0600

    Update vite.config.ts

--- Gestión de Obras Civiles/app/vite.config.ts
+++ Gestión de Obras Civiles/app/vite.config.ts
@@ -1,15 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/sismich-sistema/',
-  plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
